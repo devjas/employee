a230021124e53ba471d6319dc62186bfc7546322
Jas <30835414+devjas@users.noreply.github.com>
2022-03-30 16:32:29 -0400

    Delete manageemployees directory

--- manageemployees/src/views/partials/_footer.blade.php
+++
@@ -1,2 +0,0 @@
-<script src="{{ asset('/employee-pkg-assets/js/bootstrap.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('/employee-pkg-assets/js/all.min.js') }}" type="text/javascript"></script>
