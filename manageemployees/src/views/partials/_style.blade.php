b1225dd1a0a0cabae9922cda55ced054f18e2481
Jas <30835414+devjas@users.noreply.github.com>
2022-03-30 16:32:42 -0400

    Delete manageemployees directory

--- manageemployees/src/views/partials/_style.blade.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" href="{{ asset('/employee-pkg-assets/css/bootstrap.min.css') }}" type="text/css">
-<link rel="stylesheet" href="{{ asset('/employee-pkg-assets/css/bootstrap-reboot.min.css') }}" type="text/css">
-<link rel="stylesheet" href="{{ asset('/employee-pkg-assets/css/employee.css') }}" type="text/css">
-<link rel="stylesheet" href="{{ asset('/employee-pkg-assets/css/all.min.css') }}" type="text/css">
